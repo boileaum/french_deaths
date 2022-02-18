2e0d8721e76dc080126fc7845d6df45b221e28ae
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2022-02-18 17:20:20 +0100

    A full version

---
+++ README.md
@@ -0,0 +1,7 @@
+# Animating the French deaths
+
+The Jupyter notebook [french_deaths.ipynb](french_deaths.ipynb) describes how to visualize statistics of the French deaths over almost 5 decades.
+
+Typical output:
+
+![Animation of the age distribution over the years](deaths_wm.gif)
--- deces.py
+++ deces.py
@@ -17,10 +17,21 @@
 # ---
 
 # %% [markdown]
-# # Visualize of French deaths data
+# # Animating the French deaths
+#
+#
+# ## Introduction
+#
+# In this notebook, we use open data from the French National Institute of Statistics and Economic Studies ([INSEE](https://www.insee.fr/en/accueil)) in order to animate the age distribution of deaths over time for the last decades.
+# We also want to compare the women and men distributions.
+# We make use of [Pandas](https://pandas.pydata.org/), [Matplotlib](https://matplotlib.org/) and [seaborn](https://seaborn.pydata.org/) libraries.
 #
 # The dataset is downloaded from the "[Fichier des personnes décédés](https://www.data.gouv.fr/fr/datasets/fichier-des-personnes-decedees/)".
 # The [agregated version](https://www.data.gouv.fr/fr/datasets/fichier-des-personnes-decedees/#resource-f5465d95-e0f3-42b4-9bed-9db2c0d40261-header) (1971-2019) is the 1.9Gb file called `insee_deces.csv`.
+# This dataset is reliable on the 1971-2019 period.
+
+# %% [markdown]
+# ## A python class to process data
 #
 # Starting with some usual initializations.
 
@@ -42,14 +53,16 @@ from dataclasses import dataclass
 sns.set()
 plt.rcParams['figure.figsize'] = [10, 8]
 CSV_FILE = "insee_deces.csv"
+BY_YEAR_FILE = "deaths_by_year.png"
+DEATHS_ANIMATION_FILE = "deaths.gif"
+WM_DEATHS_ANIMATION_FILE = "deaths_wm.gif"
 BINS = 120
 
+
 # %% [markdown]
-# On se définit une fonction utilitaire pour lire de gros fichiers CSV avec Pandas.
+# We define an utilitary function for reading large CSV files with Pandas.
 
 # %%
-
-
 def read_csv_in_chunks(path, n_lines, **read_params):
     """
     A wrapper over pandas.read_csv() that prints out a progress bar
@@ -69,6 +82,10 @@ def read_csv_in_chunks(path, n_lines, **read_params):
     return df
 
 
+# %% [markdown]
+# The following dataclass will store women and men data.
+
+# %%
 @dataclass
 class Dataset:
     """Class for storing a named dataset and its visual elements"""
@@ -86,11 +103,9 @@ class Dataset:
 
 
 # %% [markdown]
-# We define a class that will handle data loading and processing.
+# We now define a class that will handle data loading, processing and plotting.
 
 # %%
-
-
 class Data:
     """A class to handle decease data"""
 
@@ -136,7 +151,7 @@ class Data:
         """Return sub dataframe corresponding to decease year"""
         return df[df['date_deces'].dt.year == year]
 
-    def plot_by_year(self, by_year_file="deces_par_annee.png"):
+    def plot_by_year(self, output_file=BY_YEAR_FILE):
         """Plot the number of deceases by year"""
         # Add an "annee_deces" column
         self.df['annee_deces'] = self.df['date_deces'].dt.year
@@ -146,48 +161,50 @@ class Data:
         by_year_women = women.groupby('annee_deces').size()
         by_year_men = men.groupby('annee_deces').size()
         ax = by_year.plot(label="Total")
-        by_year_women.plot(style='--', ax=ax, label="Femmes")
-        by_year_men.plot(style='-.', ax=ax, label="Hommes")
+        by_year_women.plot(style='--', ax=ax, label="Women")
+        by_year_men.plot(style='-.', ax=ax, label="Men")
         ax.set_ybound(lower=0)
-        ax.set_xlabel("Année")
-        ax.set_title("Nombre de décès")
+        ax.set_xlabel("Year")
+        ax.set_title("Number of deaths")
         ax.legend()
         fig = ax.get_figure()
 
-        if by_year_file:
-            fig.savefig(by_year_file)
-            print(f"Written {by_year_file}")
+        if output_file:
+            fig.savefig(output_file)
+            print(f"Written {output_file}")
         return fig, ax
 
     def plot_year(self, year: int):
         """Plot one year histogram ad return fig, ax and bar_container"""
         # Initialize figure
         fig, ax = plt.subplots()
-        ax.set_xlabel('Âge (années)')
+        ax.set_xlabel('Age of death (years)')
         _, _, bar_container = ax.hist(self.get_year(self.df, year)['age'],
                                       self.bins,
                                       range=[0, self.bins])
-        fig.suptitle("Répartition de l'âge de décès en France")
-        ax.set_title(f"Année de décès : {year}")
+        fig.suptitle("Distribution of the age of death in France")
+        ax.set_title(f"Year of death: {year}")
         ax.set_xlim([0, self.bins])
         ax.set_ylim([0, 25000])
-        ax.set_ylabel("Nombre de décès")
+        ax.set_ylabel("Number of deaths")
 
         return fig, ax, bar_container
 
-    def animate(self, output_file="deces.gif",
-                save=True) -> animation.FuncAnimation:
-        """Create and return animation"""
+    def animate(self, output_file=DEATHS_ANIMATION_FILE,
+                **kwargs) -> animation.FuncAnimation:
+        """
+        Create and return animation for the total number of deaths.
+        Save the animation if output_file is not empty
+        """
 
         def update(bar_container):
             """Update ax for animation"""
 
             def do_animate(year):
                 age = self.get_year(self.df, year)['age']
-                print(f"{year}: {len(age)}\r")
+                print(f"{year}: {len(age)}", end='\r')
                 n, _ = np.histogram(age, self.bins, range=[0, self.bins])
-                ax.set_title(f"Année du décès : {year}")
-                ax.set_xlim([0, self.bins])
+                ax.set_title(f"Year of death: {year}")
 
                 for count, rect in zip(n, bar_container.patches):
                     rect.set_height(count)
@@ -199,7 +216,7 @@ class Data:
         # Build animation
         anim = animation.FuncAnimation(fig, update(bar_container), self.years,
                                        blit=True, repeat=False)
-        if save:
+        if output_file:
             start = time.perf_counter()
             anim.save(output_file)
             end = time.perf_counter()
@@ -212,10 +229,10 @@ class Data:
         """Plot one year histogram and return fig and Datasets"""
         # Initialize figure
         fig, axes = plt.subplots(ncols=2, sharey=True)
-        fig.suptitle(f"Répartition de l'âge de décès en France : {year}")
+        fig.suptitle(f"Distribution of the age of death in France: {year}")
 
-        w = Dataset(name="Femmes", data=self.women, ax=axes[0])
-        m = Dataset(name="Hommes", data=self.men, ax=axes[1])
+        w = Dataset(name="Women", data=self.women, ax=axes[0])
+        m = Dataset(name="Men", data=self.men, ax=axes[1])
 
         for s in w, m:
             _, _, s.bar_container = s.ax.hist(
@@ -223,31 +240,36 @@ class Data:
                 self.bins,
                 range=[0, self.bins],
                 orientation='horizontal')
-            s.set_ax_params(self.bins, 15000, "Nombre de décès")
+            s.set_ax_params(self.bins, 15000, "Number of deaths")
 
         # These parameters are specific to left plot
         w.ax.invert_xaxis()
-        w.ax.set_ylabel("Âge de décès")
+        w.ax.set_ylabel("Age of death (years)")
 
         fig.tight_layout()
         return fig, (w, m)
 
-    def animate_wm(self, output_file="deces_hf.gif",
-                   save=True) -> animation.FuncAnimation:
-        """Create and return animation for women/men plots"""
+    def animate_wm(self, output_file=WM_DEATHS_ANIMATION_FILE,
+                   **kwargs) -> animation.FuncAnimation:
+        """
+        Create and return animation for women/men plots
+        Save the animation if output_file is not empty
+        """
 
         def update(wm):
             """Update ax for animation"""
 
             def do_animate(year):
                 fig.suptitle(
-                    f"Répartition de l'âge de décès en France : {year}")
+                    f"Distribution of the age of death in France: {year}")
                 for s in wm:
                     age = self.get_year(s.data, year)['age']
-                    print(f"{year} - {s.name}: {len(age)}\r")
+                    s.len = len(age)
                     n, _ = np.histogram(age, self.bins, range=[0, self.bins])
                     for count, rect in zip(n, s.bar_container.patches):
                         rect.set_width(count)
+                print(f"{year}: {wm[0].len} women, {wm[1].len} men", end='\r',
+                      flush=True)
                 return s.bar_container.patches
 
             return do_animate
@@ -256,9 +278,9 @@ class Data:
         # Build animation
         anim = animation.FuncAnimation(fig, update(wm), self.years,
                                        blit=True, repeat=False)
-        if save:
+        if output_file:
             start = time.perf_counter()
-            anim.save(output_file)
+            anim.save(output_file, **kwargs)
             end = time.perf_counter()
             print(f"Animation created in {end - start:0.1f} seconds "
                   f"and written to {output_file}")
@@ -267,24 +289,25 @@ class Data:
 
 
 # %% [markdown]
-# This main function is useful for a CLI usage
+# The following main functions are useful for a CLI usage.
 
-
-def total_by_year(csv_file=CSV_FILE, bins=BINS):
+# %%
+def total_by_year(csv_file=CSV_FILE, bins=BINS,
+                  output_file=BY_YEAR_FILE):
     """Plot total deaths by year"""
     data = Data(csv_file, bins)
-    data.plot_by_year()
+    data.plot_by_year(output_file)
 
 
 def animate_total(csv_file=CSV_FILE, bins=BINS,
-                  output_file="deces.gif"):
+                  output_file=DEATHS_ANIMATION_FILE):
     """Animate the total number deaths over the years"""
     data = Data(csv_file, bins)
     data.animate(output_file)
 
 
 def animate_wm(csv_file=CSV_FILE, bins=BINS,
-               output_file="deces_wm.gif"):
+               output_file=WM_DEATHS_ANIMATION_FILE):
     """Animate the number of women and men deaths over the years"""
     data = Data(csv_file, bins)
     data.animate_wm(output_file)
@@ -297,25 +320,52 @@ if __name__ == '__main__' and '__file__' in globals():
         'animate_total': animate_total,
         'animate_wm': animate_wm
     })
-    exit()
+    # exit because the code below is designed for being executed
+    # in a notebook
+    exit() 
+
+# %% [markdown]
+# ## Processing INSEE data
+#
+#
+# ### Data loading
+#
+# We now load the data from the CSV file.
 
 # %%
 data = Data()
 
+# %% [markdown]
+# ### Deaths over the years
+#
+# Let's plot the number of deaths over the year during the target period.
+
 # %%
-# data.plot_by_year(by_year_file=None)
-data.plot_by_year()
+data.plot_by_year(output_file=None);
 
 # %% [markdown]
-# We create an animation object
+# ### Animating the total deaths
+#
+# We loop over the 1971-2019 period in order to create a matplotlib animation object then we display it.
 
 # %%
-anim = data.animate(save=False)
-
-# %%
-video = anim.to_html5_video()
-
-# %%
+anim = data.animate(output_file=None)
+video = anim.to_jshtml(default_mode='loop')
+plt.close()
 HTML(video)
 
+# %% [markdown]
+# ### Comparing women and men
+
 # %%
+anim_wm = data.animate_wm(output_file=None)
+video_wm = anim_wm.to_jshtml(default_mode='loop')
+plt.close()
+HTML(video_wm)
+
+# %% [markdown]
+# We finally save the animation to a gif file.
+
+# %%
+data.animate_wm(fps=12)
+plt.close()
